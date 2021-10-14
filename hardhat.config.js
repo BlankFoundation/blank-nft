f2a3ef9ee00d7331e1c7c95dcdbe93bde438b1b8
Ford <ford@thegraph.com>
2021-10-13 17:06:31 -0700

    Update hardhat setup.. update config, add extensions, add scripts
    - Extensions: abi-exporter, contract-sizer, gas-reporter, typechain

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
+require('hardhat-contract-sizer');
+require('hardhat-abi-exporter');
+require('hardhat-gas-reporter');
+require('@typechain/hardhat');
+require('@nomiclabs/hardhat-ethers');
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,5 +22,61 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  paths: {
+    sources: './contracts',
+    tests: './test',
+    artifacts: './build/contracts',
+  },
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.4',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+          outputSelection: {
+            '*': {
+              '*': ['storageLayout'],
+            },
+          },
+        },
+      },
+    ],
+  },
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      loggingEnabled: false,
+      gas: 12000000,
+      gasPrice: 'auto',
+      blockGasLimit: 12000000,
+      mining: {
+        auto: true,
+        interval: 30000,
+      },
+      hardfork: 'london',
+    },
+  },
+  typechain: {
+    outDir: 'build/types',
+    target: 'ethers-v5',
+  },
+  abiExporter: {
+    path: './build/abis',
+    clear: false,
+    flat: true,
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: false,
+    disambiguatePaths: false,
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS ? true : false,
+    showTimeSpent: true,
+    currency: 'USD',
+  },
 };
