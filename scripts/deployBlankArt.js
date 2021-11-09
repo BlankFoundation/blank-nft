507c5563cf199983d3b7da1695c9e851b0d7cb50
Ford <ford@thegraph.com>
2021-11-09 14:43:35 -0800

    Apply formatting suggestions from lint and prettier

--- scripts/deployBlankArt.js
+++ scripts/deployBlankArt.js
@@ -1,16 +1,16 @@
 async function main() {
-  const [deployer] = await ethers.getSigners();
+  const [deployer] = await ethers.getSigners()
 
-  console.log("Deploying contracts with the account:", deployer.address);
+  console.log('Deploying contracts with the account:', deployer.address)
 
-  console.log("Account balance:", (await deployer.getBalance()).toString());
+  console.log('Account balance:', (await deployer.getBalance()).toString())
 
-  const BlankArt = await ethers.getContractFactory("BlankArt")
-  const blankArt = await BlankArt.deploy(deployer.address, 10000);
+  const BlankArt = await ethers.getContractFactory('BlankArt')
+  const blankArt = await BlankArt.deploy(deployer.address, 10000)
 
   // Start deployment, returning a promise that resolves to a contract object
-  await blankArt.deployed();
-  console.log("Contract deployed to address:", blankArt.address);
+  await blankArt.deployed()
+  console.log('Contract deployed to address:', blankArt.address)
 }
 
 main()
